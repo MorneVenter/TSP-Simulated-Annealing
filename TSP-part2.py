4713da2cadcc5093353fddbec9d8b1f0be29f84d
Morne Venter <morneventer@pop-os.localdomain>
2020-10-19 19:40:34 +0200

    added new heuristic

--- TSP-part2.py
+++ TSP-part2.py
@@ -20,12 +20,7 @@ plt.rcParams['axes.prop_cycle'] = plt.cycler(color=color_list)
 plt.rcParams['figure.facecolor'] = '#0d1a26'
 #------------------------------------------------------------------------------#
 # Variables
-pixel_stride = 5 #Length of pixel sampling.
-temp = 80 #start temperature
-alpha = 0.993 #alpha value
-temp_init = temp #ref to start temperature
-markov_chain_count = 2000 #Number of markov chains
-markov_chain_length = 500 #Length of markov chains
+pixel_stride = 2 #Length of pixel sampling.
 #------------------------------------------------------------------------------#
 def clear():
 
@@ -48,20 +43,12 @@ class Coordinate:
     def get_distance(a ,b):
         return np.sqrt(np.abs(a.x-b.x) + np.abs(a.y-b.y))
 
-    @staticmethod
-    def get_total_distance(all_pts):
-        dist = 0
-        for pt1, pt2 in zip(all_pts[:-1], all_pts[1:]):
-            dist += Coordinate.get_distance(pt1, pt2)
-        dist += Coordinate.get_distance(all_pts[0], all_pts[-1])
-        return dist
-
 #------------------------------------------------------------------------------#
 
 #----------------------------Get Image-----------------------------------------#
 img = cv2.imread('test.jpg')
 grayImage = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-(thresh, blackAndWhiteImage) = cv2.threshold(grayImage, 30, 255, cv2.THRESH_BINARY_INV)
+(thresh, blackAndWhiteImage) = cv2.threshold(grayImage, 145, 255, cv2.THRESH_BINARY)
 
 # cv2.imshow("Image",blackAndWhiteImage)
 # cv2.waitKey(0)
@@ -72,52 +59,45 @@ all_pts = []
 for x in range(0, height, pixel_stride):
     for y in range(0, width, pixel_stride):
         r = random.random()
-        if blackAndWhiteImage[x,y] == 255 and r < 0.3:
+        if blackAndWhiteImage[x,y] == 255 and r < 0.6:
             all_pts.append(Coordinate(y,-x))
 
 #------------------------------------------------------------------------------#
-#-------------------------------Annealing--------------------------------------#
-cost0 = Coordinate.get_total_distance(all_pts)
+#-------------------TSP Nearest Neighbour--------------------------------------#
+solution = []
+current_pt = 0
+next_pt = np.random.randint(0, len(all_pts))
+solution.append(all_pts[current_pt])
 
-for i in range(markov_chain_count):
+while(len(solution) != len(all_pts)):
     clear()
-    print(i,'/',markov_chain_count,': Total Length = ', cost0)
-    temp = temp*alpha
+    print(str(round(len(solution)/len(all_pts)*100.0, 2)),'%')
+    for i in range(0,len(all_pts)):
+        if i != current_pt:
+            dist = Coordinate.get_distance(all_pts[current_pt], all_pts[i])
+            curdist = Coordinate.get_distance(all_pts[current_pt], all_pts[next_pt])
+            if dist < curdist and not(all_pts[i] in solution):
+                next_pt = i
 
-    for j in range(markov_chain_length):
-        #swap two points
-        r1, r2 = np.random.randint(0, len(all_pts), size=2)
-        tmp = all_pts[r1]
-        all_pts[r1] = all_pts[r2]
-        all_pts[r2] = tmp
-
-        cost1 = Coordinate.get_total_distance(all_pts)
-
-        if cost1 < cost0:
-            cost0 = cost1
-        else:
-            x = np.random.uniform()
-            if x < np.exp((cost0-cost1)/temp):
-                cost0 = cost1
-            else:
-                tmp = all_pts[r1]
-                all_pts[r1] = all_pts[r2]
-                all_pts[r2] = tmp
+    solution.append(all_pts[next_pt])
+    current_pt = next_pt
+    next_pt = np.random.randint(0, len(all_pts))
 
 #------------------------------------------------------------------------------#
 #----------------------------Plotting------------------------------------------#
-fig = plt.figure(figsize=(7,7))
+fig = plt.figure(figsize=(4,8))
 ax = fig.add_subplot()
-fig.canvas.set_window_title('Annealing')
+fig.canvas.set_window_title('TSP')
 linecolor = CB91_Blue
-dotcolor = '#ffffff'
 
-for pt1, pt2 in zip(all_pts[:-1], all_pts[1:]):
-    ax.plot([pt1.x, pt2.x], [pt1.y, pt2.y], linecolor)
-ax.plot([all_pts[0].x,all_pts[-1].x],[all_pts[0].y,all_pts[-1].y], linecolor)
-# for pt in all_pts:
-#     ax.plot(pt.x,pt.y, marker='o', color=dotcolor, markersize=1)
+for pt1, pt2 in zip(solution[:-1], solution[1:]):
+    ax.plot([pt1.x, pt2.x], [pt1.y, pt2.y], linecolor, linewidth=0.5)
+#ax.plot([all_pts[0].x,all_pts[-1].x],[all_pts[0].y,all_pts[-1].y], linecolor, linewidth=0.5)
 
+# for pt in solution:
+#     ax.plot(pt.x,pt.y, marker='o', color=dotcolor, markersize=0.75)
+
+plt.axis('equal')
 ax.axis('tight')
 ax.axis('off')
 plt.show()
